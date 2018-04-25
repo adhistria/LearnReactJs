060dd0c1f26654bd744f15253723d7a12c33a032
Adhi Satria <adhisatria1@outlook.com>
2018-04-25 11:31:42 +0700

    first Commit

---
+++ src/App.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Person from './Person/Person';
+class App extends Component {
+    state = {
+        persons:[
+            {name: "Adhi", age:"22"},
+            {name: "Mirah", age:"21"}
+        ],
+        showPersons : false
+    };
+
+    switchNameHandler = () => {
+        this.setState({
+            persons:[
+                {name: "Tes", age:"22"},
+                {name: "Mirah", age:"21"}
+            ]}
+        );
+    };
+    nameChangedHandler = (event) =>{
+        this.setState({
+            persons:[
+                {name: event.target.value, age:"22"},
+                {name: "Mirah", age:"21"}
+            ]}
+        );
+    };
+    togglePersonHandler = () => {
+        const doesShow = this.state.showPersons;
+        this.setState({
+            showPersons : !doesShow
+        });
+    };
+  render() {
+    const style = {
+        backgroudColor :'white',
+        font : 'inherit',
+        border : '2px solid #aaa',
+        padding : '8px'
+    };
+    let persons = null;
+    if(this.state.showPersons){
+        persons = (
+            <div>
+                {this.state.persons.map(person=>{
+                    return <Person>
+                    name={person.name}
+                    age={person.age}
+                    </Person>
+                })}
+                {/*<Person*/}
+                    {/*name={this.state.persons[0].name}*/}
+                    {/*age={this.state.persons[0].age}*/}
+                    {/*changed = {this.nameChangedHandler}*/}
+                {/*>Coba euy*/}
+                {/*</Person>*/}
+                {/*<Person*/}
+                    {/*name={this.state.persons[1].name}*/}
+                    {/*age={this.state.persons[1].age}*/}
+                    {/*changed = {this.nameChangedHandler}*/}
+                {/*>*/}
+                    {/*Coba euy*/}
+                {/*</Person>*/}
+            </div>
+        );
+
+
+    }
+    return (
+      <div className="App">
+          <h1>Halo</h1>
+          {/*<button onClick={this.switchNameHandler.bind(this,'Adhi euy')}>Switch Name</button>*/}
+          {/*gunain bind dibandingkan fungsi di dalam onClick*/}
+
+          {/*<button style =  {style} onClick={() => this.switchNameHandler('Maximilian')}>Switch Name</button>*/}
+          <button style =  {style} onClick={this.togglePersonHandler}>Toggle Persons</button>
+          {persons}
+          {/*{this.state.showPersons ?*/}
+              {/*<div>*/}
+              {/*<Person*/}
+                  {/*name={this.state.persons[0].name}*/}
+                  {/*age={this.state.persons[0].age}*/}
+                  {/*changed = {this.nameChangedHandler}*/}
+              {/*>Coba euy*/}
+              {/*</Person>*/}
+              {/*<Person*/}
+                  {/*name={this.state.persons[1].name}*/}
+                  {/*age={this.state.persons[1].age}*/}
+                  {/*changed = {this.nameChangedHandler}*/}
+              {/*>*/}
+                  {/*Coba euy*/}
+              {/*</Person>*/}
+              {/*</div>*/}
+              {/*: null*/}
+          {/*}*/}
+          {/*<Person*/}
+              {/*changed = {this.nameChangedHandler}*/}
+              {/*name={this.state.persons[0].name}*/}
+              {/*age={this.state.persons[0].age}*/}
+              {/*click={this.switchNameHandler.bind(this,'COBA')}*/}
+          {/*/>*/}
+          {/*<Person*/}
+              {/*changed = {this.nameChangedHandler}*/}
+              {/*name={this.state.persons[1].name}*/}
+              {/*age={this.state.persons[1].age}*/}
+              {/*click={this.switchNameHandler.bind(this,'COBA')}*/}
+          {/*/>*/}
+          {/*<Person name = "Tes" age="17" changed = {this.nameChangedHandler}/>*/}
+          {/*<Person/>*/}
+      </div>
+    );
+    //   return(
+    //       React.createElement("div", {className:"App"},React.createElement("h1",null,"halo"))
+    //   );
+
+  }
+}
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Person.css';
+const Person = (props) =>{
+    // return <p> Test Tjuy{Math.floor(Math.random()*30)}</p>
+    return(
+        <div className="Person">
+            <p onClick={props.click}>Name {props.name}  : {props.age}</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
+        </div>
+
+        )
+
+
+    // render(){
+    //     return{
+    //
+    //     }
+    // }
+
+};
+export default Person;
