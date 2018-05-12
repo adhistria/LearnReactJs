8830a71d7b871fc9cd576cc10ef70b1a2fc02924
Adhi Satria <adhisatria1@outlook.com>
2018-05-12 15:47:21 +0700

    change stateless to statefull

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React , {Component} from 'react';
 import classes from './Person.css';
-const Person = (props) =>{
-    // return <p> Test Tjuy{Math.floor(Math.random()*30)}</p>
-    // const rnd = Math.random();
-    // if(rnd > 0.9){
-    //     throw new Error('Error euy');
-    // }
-    //ga pake error boundary lagi
 
-    return(
-        <div className={classes.Person} >
-            <p onClick={props.click}>Nama {props.name} , Umur : {props.age}</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name}/>
-        </div>
-
-        )
-
-
-    // render(){
-    //     return{
-    //
-    //     }
-    // }
-
-};
+class Person extends Component{
+    render(){
+        return(
+            <div className={classes.Person} >
+                <p onClick={this.props.click}>Nama {this.props.name} , Umur : {this.props.age}</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+            </div>
+        );
+    }
+}
+// const Person = (props) =>{
+//     // return <p> Test Tjuy{Math.floor(Math.random()*30)}</p>
+//     // const rnd = Math.random();
+//     // if(rnd > 0.9){
+//     //     throw new Error('Error euy');
+//     // }
+//     //ga pake error boundary lagi
+//
+//     return(
+//
+//
+//         )
+//
+//
+//     // render(){
+//     //     return{
+//     //
+//     //     }
+//     // }
+//
+// };
 export default Person;
